28ffd713ce607c57aa33a2800137be04515f5bbf
julian-ferrari <julianferrarijuli@gmail.com>
2025-05-05 21:59:06 +0000

    Realizados ejercicios restantes Practica 2

--- ejercicios/practica2.py
+++ ejercicios/practica2.py
@@ -59,20 +59,17 @@ def componentes_conexas(grafo_lista):
     '''
     nodos, aristas = grafo_lista
 
-    # Crear el grafo como un diccionario vacío
     grafo = {}
     for nodo in nodos:
         grafo[nodo] = []
 
-    # Agregar las conexiones (aristas) al grafo
     for a, b in aristas:
         grafo[a].append(b)
-        grafo[b].append(a)  # porque el grafo es no dirigido
+        grafo[b].append(a)
 
     visitados = []
     componentes = []
 
-    # Recorrer cada nodo
     for nodo in nodos:
         if nodo not in visitados:
             componente = []
@@ -94,4 +91,70 @@ def es_conexo(grafo_lista):
     Dado un grafo en representacion de lista, y utilizando la función "componentes_conexas"
     devuelve True/False si el grafo es o no conexo.
     '''
-    pass
+    componentes = componentes_conexas(grafo_lista)
+    return len(componentes) == 1
+
+def es_completo(grafo_lista):
+    '''
+    Dado un grafo en representacion de lista, devuelve True/False si el grafo es o no completo.
+    Ejemplo Entrada:
+    	['3', 'A', 'B', 'C', 'A B', 'B A', 'A C', 'C A', 'B C', 'C B']
+    Ejemplo formato salida:
+    	True
+    '''
+    vertices, aristas = grafo_lista
+    n = len(vertices)
+    e = len(aristas)
+
+    max_aristas = ((n * (n+1)) / 2)
+
+    return e == max_aristas
+    	
+def aristas_de(grafo, vertice):
+    '''
+    Dado un grafo en representacion de lista, devuelva todas las aristas salientes desde un vértice dado
+    Ejemplo Entrada:
+    	grafo = ['3', 'A', 'B', 'C', 'A B', 'A C', 'B C']
+	aristas_de(grafo, 'A')
+    Ejemplo formato salida:
+    	[('A', 'B'), ('A', 'C')]
+    '''
+    vertices, aristas = grafo
+    return [arista for arista in aristas if arista[0] == vertice]
+
+def grafo_inducido(grafo, subconjunto_vertices):
+    '''
+    Dado un grafo en representacion de lista, y un subconjunto de vertices,
+    devuelva el subgrafo inducido
+    Ejemplo Entrada:
+    	grafo = ['4', 'A', 'B', 'C', 'D', 'A B', 'A C', 'B D']
+	subconjunto_vertices = ['A', 'B', 'C']
+    Ejemplo formato salida:
+    	(['A', 'B', 'C'], [('A', 'B'), ('A', 'C')])
+    '''
+    vertices, aristas = grafo
+    
+    aristas_inducidas = [arista for arista in aristas if arista[0] in subconjunto_vertices and arista[1] in subconjunto_vertices]
+    return (subconjunto_vertices, aristas_inducidas)
+
+def grafo_complementario(grafo):
+    '''
+    Dado un grafo en representacion de lista, devuelve el grafo complementario en forma de lista
+    Ejemplo Entrada:
+    	['3', 'A', 'B', 'C', 'A B', 'B C']
+    Ejemplo formato salida:
+    	(['A', 'B', 'C'], [('A', 'C'), ('B', 'A'), ('C', 'A'), ('C', 'B')])
+    '''
+    vertices, aristas = grafo
+    aristas_set = set(aristas)
+
+    complemento_aristas = []
+
+    for i in range(len(vertices)):
+        for j in range(i + 1, len(vertices)):
+            v1 = vertices[i]
+            v2 = vertices[j]
+            if (v1, v2) not in aristas_set and (v2, v1) not in aristas_set:
+                complemento_aristas.append((v1, v2))
+                
+    return (vertices, complemento_aristas)
